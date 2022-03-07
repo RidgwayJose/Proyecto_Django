396e87c1de42d0f34518a32e8c28e623fbc1a023
RidgwayJose <ridgwayjose39@gmail.com>
2022-03-06 23:21:00 -0500

    Conexion con PostgreSQL

--- crud/admin.py
+++ crud/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 from crud.models import Product
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+class ProductoResource(resources.ModelResource):
+  class Meta:
+    model = Product
 
 @admin.register(Product)
-class Admin(admin.ModelAdmin):
+class ProductAdmin(ImportExportModelAdmin ,admin.ModelAdmin):
   search_fields = ['id', 'name']
   list_display = ['f_id', 'name', 'brand', 'create']
   ordering = ['id']
-  list_filter= ['name', 'brand','category']  
+  list_filter= ['name', 'brand','category']
+  resources_class = ProductoResource  
 
 # Register your models here.
 #admin.site.register(Product)
