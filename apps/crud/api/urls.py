3e4369928105662e4f78f7f9c8ca2bd28b58a2aa
RidgwayJose <ridgwayjose39@gmail.com>
2022-03-17 03:50:12 -0500

    Version 2.0

--- apps/crud/api/urls.py
+++ apps/crud/api/urls.py
@@ -3,6 +3,6 @@ from apps.crud.api.views.general_views import ProductListAPiView
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('prod/', login_required(ProductListAPiView.as_view()), name = 'productAPI_list_view')
+    path('prod/', ProductListAPiView.as_view(), name = 'productAPI_list_view')
     #path('producto/', ProductAPIView.as_view(), name = 'producto_api')
 ]
